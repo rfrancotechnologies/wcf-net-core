c7735c68910498aa88f5bcfea88ee222c0dc4451
jsolana_cr <jsolana@rfranco.com>
2020-04-02 12:46:22 +0200

    Added to namespace Com.RFranco.Wcf.Audit.Log4Net.

--- wcf-audit-log4net/Log4NetAuditBehavior.cs
+++ wcf-audit-log4net/Log4NetAuditBehavior.cs
@@ -5,62 +5,66 @@ using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
 using System.Xml;
 
-public class Log4NetAuditBehavior : IEndpointBehavior
+namespace Com.RFranco.Wcf.Audit.Log4Net
 {
-    public void AddBindingParameters(
-        ServiceEndpoint endpoint,
-        System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+    public class Log4NetAuditBehavior : IEndpointBehavior
     {
-    }
-
-    public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
-    {
-    }
-
-    public void Validate(ServiceEndpoint endpoint)
-    {
-    }
-
-    public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
-    {
-        clientRuntime.ClientMessageInspectors.Add(new Log4NetAuditMessageInspector());
-    }
-
-    private class Log4NetAuditMessageInspector : IClientMessageInspector
-    {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        public void AfterReceiveReply(ref Message reply, object correlationState)
+        public void AddBindingParameters(
+            ServiceEndpoint endpoint,
+            System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
         {
-            var buffer = reply.CreateBufferedCopy(Int32.MaxValue);
-            reply = buffer.CreateMessage();
-            LogMessage(buffer, false);
         }
 
-        public object BeforeSendRequest(ref Message request, System.ServiceModel.IClientChannel channel)
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
         {
-            var buffer = request.CreateBufferedCopy(Int32.MaxValue);
-            request = buffer.CreateMessage();
-            LogMessage(buffer, true);
-            return request;
         }
 
-        private void LogMessage(MessageBuffer buffer, bool isRequest)
+        public void Validate(ServiceEndpoint endpoint)
         {
-            var originalMessage = buffer.CreateMessage();
-            string messageContent;
+        }
 
-            using (StringWriter stringWriter = new StringWriter())
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.ClientMessageInspectors.Add(new Log4NetAuditMessageInspector());
+        }
+
+        private class Log4NetAuditMessageInspector : IClientMessageInspector
+        {
+            private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            public void AfterReceiveReply(ref Message reply, object correlationState)
             {
-                using (XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter))
-                {
-                    originalMessage.WriteMessage(xmlTextWriter);
-                    xmlTextWriter.Flush();
-                    xmlTextWriter.Close();
-                }
-                messageContent = stringWriter.ToString();
+                var buffer = reply.CreateBufferedCopy(Int32.MaxValue);
+                reply = buffer.CreateMessage();
+                LogMessage(buffer, false);
             }
-            
-            log.DebugFormat("Processing {0}: {1}", (isRequest ? "Request" :"Response"), messageContent);
+
+            public object BeforeSendRequest(ref Message request, System.ServiceModel.IClientChannel channel)
+            {
+                var buffer = request.CreateBufferedCopy(Int32.MaxValue);
+                request = buffer.CreateMessage();
+                LogMessage(buffer, true);
+                return request;
+            }
+
+            private void LogMessage(MessageBuffer buffer, bool isRequest)
+            {
+                var originalMessage = buffer.CreateMessage();
+                string messageContent;
+
+                using (StringWriter stringWriter = new StringWriter())
+                {
+                    using (XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter))
+                    {
+                        originalMessage.WriteMessage(xmlTextWriter);
+                        xmlTextWriter.Flush();
+                        xmlTextWriter.Close();
+                    }
+                    messageContent = stringWriter.ToString();
+                }
+
+                log.DebugFormat("Processing {0}: {1}", (isRequest ? "Request" : "Response"), messageContent);
+            }
         }
     }
 }
+
